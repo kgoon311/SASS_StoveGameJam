ecc0a8376c2c5628ddafd18f1bf437fc553e94c7
kgoon <sdh210203@sdh.hs.kr>
2022-08-27 13:30:55 +0900

    save
    push

--- SASS_StoveGameJam/Assets/KimDongJin/Script/Ending.cs
+++ SASS_StoveGameJam/Assets/KimDongJin/Script/Ending.cs
@@ -4,26 +4,32 @@ using UnityEngine;
 
 public class Ending : MonoBehaviour
 {
-    [SerializeField] GameObject PushFloor;//보급품 오브젝트를 위로 밀어주는 오브젝트
-    [SerializeField] List<GameObject> contents = new List<GameObject>();
+    [SerializeField] List<GameObject> ItemObjects = new List<GameObject>();//보급품 오브젝트
+    private List<Item> ItemList = new List<Item>();
+    private int ItemIdx;
     void Start()
     {
         StartCoroutine(MoveUp());
+        foreach (GameObject Item in ItemObjects)
+        {
+            ItemIdx++;
+            Item.GetComponent<SpriteRenderer>().sprite = null;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     IEnumerator MoveUp()
     {
         float Uptimer = 0;
         Rigidbody2D FloorRG = GetComponent<Rigidbody2D>();
-        while(Uptimer < 1)
+        while (Uptimer < 1)
         {
-            Uptimer += Time.deltaTime*2;
-            FloorRG.velocity = Vector3.up*20;
+            Uptimer += Time.deltaTime * 2;
+            FloorRG.velocity = Vector3.up * 20;
 
             yield return null;
         }
